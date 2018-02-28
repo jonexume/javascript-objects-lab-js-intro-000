62b9db0f3803b9296f0ffcd2001e49e0d295a7c4
Jon Exume <jonexume@gmail.com>
2018-02-28 05:43:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(recipe, key, value){
-  return recipe;
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
 
-destructivelyUpdateObjectWithKeyAndValue(recipe, ‘key’, ‘value’)
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj.key;
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
